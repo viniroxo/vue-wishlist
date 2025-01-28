6036e4dfc218a4097eace3b9f2895fbf95851a26
Vinicius Roxo Brandão <viniroxo@gmail.com>
2025-01-28 10:32:50 -0300

    Initial commit and configurations

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import vue from 'eslint-plugin-vue';
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import vueParser from 'vue-eslint-parser';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx', '**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: typescriptParser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      vue,
+      '@typescript-eslint': typescriptPlugin,
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+];
